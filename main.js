2109783978a3cd5e5505f63780c02c98abe5105c
B1lol-dev <sardorovichgamer@gmail.com>
2024-08-24 21:23:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const navCollection = document.getElementById('nav_collection');
+const navClose = document.getElementById('nav_close');
+const navMenu = document.getElementById('nav_menu');
+
+let if_nav_open = false;
+
+navMenu.addEventListener('click' , () =>{
+    if (!if_nav_open) {
+        navCollection.style.transform='translateX(0)';
+        if_nav_open = true;
+    } else{
+        navCollection.style.transform='';
+        if_nav_open = false;
+    }
+})
+
+navClose.addEventListener('click' , () =>{
+    if (!if_nav_open) {
+        navCollection.style.transform='translateX(0)';
+        if_nav_open = true;
+    } else{
+        navCollection.style.transform='';
+        if_nav_open = false;
+    }
+})
